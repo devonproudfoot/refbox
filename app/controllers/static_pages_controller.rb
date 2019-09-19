7d36900556137b3a968eff316098c03a6b8227ad
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-09-19 18:44:59 +0000

    Trying to make a page for reports, button does not work

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,7 @@ class StaticPagesController < ApplicationController
   def index
   end
 
+  def generate_reports
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "static_pages#index"
 
+  get 'generate_reports', to: 'static_pages#generate_reports'
+
   resources :requests, only: [:new, :create, :show]
   resource :dashboard, only: [:show]
 
